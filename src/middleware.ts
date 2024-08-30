241ef5d5cad0eaa6759e6762b39a862bbf2b6d8b
Rajdeep Chowdhury <rajdeepchowdhury318@gmail.com>
2024-08-30 23:50:21 +0530

    fewfewqfewf

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,21 @@
-export { auth as middleware } from "@/providers/Auth"
+import NextAuth from 'next-auth';
 
+
+import { authOptions } from './providers/Auth';
+
+const { auth } = NextAuth(authOptions);
+
+export default auth((req:any) => {
+ const { nextUrl } = req;
+
+ const isAuthenticated = !!req.auth;
+ 
+ if (isAuthenticated)
+  return Response.redirect(new URL("/", nextUrl));
+
+ if (!isAuthenticated)
+  return Response.redirect(new URL("/", nextUrl));
+});
 
 export const config = { 
       matcher: [
